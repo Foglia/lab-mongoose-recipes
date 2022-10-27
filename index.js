e73d24d2ef2df41d97b031a8c565fdf08989f9e0
Flavia Fogliato <flavia.fogliato@gmail.com>
2022-10-27 16:53:13 +0100

    lab-done

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const ratatouille = { 
+  title:'Ratatouille', 
+  level:'Easy Peasy', 
+  cuisine: 'French',
+  dishType: 'other',
+  image: 'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?output-format=auto&output-quality=auto&resize=600:*',
+  duration: 45,
+}
+
 //Method 1 : Using Async Await
 
 const manageRecipes = async () => {
@@ -17,8 +26,19 @@ const manageRecipes = async () => {
 
     // Before adding any recipes to the database, let's remove all existing ones
     await Recipe.deleteMany();
-
+    await Recipe.create(ratatouille);
     // Run your code here, after you have insured that the connection was made
+    let manyRecipes = await Recipe.insertMany(data);
+    manyRecipes.forEach((Recipes => console.log(Recipes.title)));
+
+    let updateRecipe = await Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, {duration: 100});
+    console.log(`Rigatoni alla Genovese updated`);
+
+    await Recipe.deleteOne({ title: 'Carrot Cake' });
+    console.log(`Carrot Cake deleted`)
+
+    mongoose.disconnect();
+    
   } catch (error) {
     console.log(error);
   }
